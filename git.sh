aa4bff4e070eefef84f68b4981a10f50f5baee57
Greg Myers <g.myers.10@aberdeen.ac.uk>
2013-09-04 10:16:12 +0100

    added gitco

--- git.sh
+++ git.sh
@@ -4,6 +4,8 @@ alias gs='git status'
 alias glo='git log --oneline'
 alias gl="git log --graph --pretty=\"format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset\""
 
+git config --global alias.co checkout
+
 # Set git autocompletion and PS1 integration
 if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
   . /usr/local/git/contrib/completion/git-completion.bash
