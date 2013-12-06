d056d8909b836ceef284b8fb4bbf6b1191a05562
Greg Myers <g.myers.10@aberdeen.ac.uk>
2013-12-06 13:45:40 +0000

    commiting a habit

--- git.sh
+++ git.sh
@@ -1,8 +1,10 @@
 alias gc='git commit -m'
+alias gca='git commit -am'
 alias ga='git add'
 alias gs='git status -sbuall'
 alias glo='git log --oneline'
 alias gl="git log --graph --pretty=\"format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset\""
+alias gds='git diff --staged'
 alias gd="git diff"
 
 git config --global alias.co checkout
