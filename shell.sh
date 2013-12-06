ab076b3345cdb83f73657f8105f93935d5131404
Greg Myers <g.myers.10@aberdeen.ac.uk>
2013-12-06 10:30:32 +0000

    linked sub to subl, the standard command

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@ alias ll='ls -ltr | tail'
 alias l='ls -1tr | tail'
 alias fndr='open /System/Library/CoreServices/Finder.app "$PWD"'
 
-alias sub='sublime'
+alias sub='subl'
 
 PS1='\[\033[0;33m\]$(rvm current)\[\033[0m\]:$(basename $PWD) \[\033[0m\]$ '
 CLICOLOR=1; export CLICOLOR
