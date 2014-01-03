92d45d4048e739d9df38048c13f5aa9ec8f3a74b
Greg Myers <g.myers.10@aberdeen.ac.uk>
2014-01-03 14:25:22 +0000

    removed detection of autocomplete

--- git.sh
+++ git.sh
@@ -9,10 +9,6 @@ alias gd="git diff"
 
 git config --global alias.co checkout
 
-# Set git autocompletion and PS1 integration
-if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
-  . /usr/local/git/contrib/completion/git-completion.bash
-fi
 export GIT_PS1_SHOWDIRTYSTATE=true
 
 PS1='\[\033[32m\]$(rvm current)\[\033[00m\]:\[\033[34m\]$(basename $PWD)\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
