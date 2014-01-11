d7a4176d305aed8f82bd69d4ec2581539124c540
Greg Myers <neonmd@hotmail.com>
2014-01-11 18:54:26 +0000

    using users git-prompt and git-completion from common locations, turning git colour by default, and loading git-prompt in git because it complaned about __git_ps1

--- git.sh
+++ git.sh
@@ -7,8 +7,10 @@ alias gl="git log --graph --pretty=\"format:%C(yellow)%h%Cblue%d%Creset %s %C(wh
 alias gds='git diff --staged'
 alias gd="git diff"
 
+git config --global color.ui true
 git config --global alias.co checkout
 
 export GIT_PS1_SHOWDIRTYSTATE=true
 
+. ./git-prompt.sh
 PS1='\[\033[32m\]$(rvm current)\[\033[00m\]:\[\033[34m\]$(basename $PWD)\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
