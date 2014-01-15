d1369e4063b2043c8a0d22e4424fd7a8495be26e
Greg Myers <g.myers.10@aberdeen.ac.uk>
2014-01-15 10:18:24 +0000

    added reload method for sourcing bash_profile

--- shell.sh
+++ shell.sh
@@ -1,7 +1,7 @@
 alias ll='ls -ltr | tail'
 alias l='ls -1tr | tail'
 alias fndr='open /System/Library/CoreServices/Finder.app "$PWD"'
-
+alias reload='source ~/.bash_profile'
 alias sub='subl'
 
 PS1='\[\033[0;33m\]$(rvm current)\[\033[0m\]:\W\[\033[0m\]$ '
