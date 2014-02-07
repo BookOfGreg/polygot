8ebfb7c9cfb883ed78e3d689504236eaa271b96c
Greg Myers <g.myers.10@aberdeen.ac.uk>
2014-02-07 15:32:42 +0000

    removed db:seed from rbd, fails on project

--- rails.sh
+++ rails.sh
@@ -1,2 +1,2 @@
 alias rdbm='echo Migrating db and prepping test db; bundle exec rake db:migrate db:test:prepare'
-alias rdb='echo Dropping db, create, migrate, seed and test prepare;bundle exec rake db:drop db:create db:migrate db:seed db:test:prepare'
+alias rdb='echo Dropping db, create, migrate, and test prepare;bundle exec rake db:drop db:create db:migrate db:test:prepare'
