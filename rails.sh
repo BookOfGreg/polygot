aa35d77370ed3bc2fac6005fc1aaffbdabeb727e
Greg Myers <g.myers.10@aberdeen.ac.uk>
2014-02-24 09:52:58 +0000

    Making spree-safe db:drop to seed

--- rails.sh
+++ rails.sh
@@ -1,2 +1,2 @@
 alias rdbm='echo Migrating db and prepping test db; bundle exec rake db:migrate db:test:prepare'
-alias rdb='echo Dropping db, create, migrate, and test prepare;bundle exec rake db:drop db:create db:migrate db:test:prepare'
+alias rdb='echo Dropping db, create, migrate, and test prepare, then seed;bundle exec rake db:drop db:create db:migrate db:test:prepare; bundle exec rake db:seed'
