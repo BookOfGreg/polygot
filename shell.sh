ba168a1307c2c36f4fae0151ce0d96cea580cfea
Greg Myers <neonmd@hotmail.co.uk>
2014-10-16 12:29:45 +0100

    Added python server

--- shell.sh
+++ shell.sh
@@ -3,6 +3,7 @@ alias l='ls -1tr | tail'
 alias fndr='open /System/Library/CoreServices/Finder.app "$PWD"'
 alias reload='source ~/.bash_profile'
 alias sub='subl'
+alias serve='python -m SimpleHTTPServer'
 
 PS1='\[\033[0;33m\]$(ruby -v | cut -d " " -f 2)\[\033[0m\]:\W\[\033[0m\]$ '
 CLICOLOR=1; export CLICOLOR
