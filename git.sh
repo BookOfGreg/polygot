2eadda5cbb288bc527daf89d99e8963677434e1f
Greg Myers <neonmd@hotmail.co.uk>
2015-10-09 09:59:51 +0100

    better glo, use ruby not python for httpd, js ps1 available

--- git.sh
+++ git.sh
@@ -16,7 +16,7 @@ alias gc='git commit -m'
 alias gca='git commit -am'
 alias ga='git add'
 alias gs='git status -sbuall'
-alias glo='git log --oneline'
+alias glo='git log --oneline --decorate'
 alias gl="git log --graph --pretty=\"format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset\""
 alias gds='git diff --staged'
 alias gd="git diff"
--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ alias l='ls -al'
 alias fndr='open /System/Library/CoreServices/Finder.app "$PWD"'
 alias reload='source ~/.bash_profile'
 alias sub='subl'
-alias serve='python -m SimpleHTTPServer'
+alias serve='ruby -run -e httpd . -p 9090'
 
 PS1='\[\033[0;33m\]$(ruby -v | cut -d " " -f 2)\[\033[0m\]:\W\[\033[0m\]$ '
 CLICOLOR=1; export CLICOLOR
