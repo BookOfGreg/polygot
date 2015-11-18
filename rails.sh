4735ba0b0fdeb2ddd92f1dbe447d784799311659
Greg Myers <neonmd@hotmail.co.uk>
2015-11-18 09:59:57 +0000

    easier rake tests

--- rails.sh
+++ rails.sh
@@ -1,4 +1,13 @@
+function rake_test_with_test_name {
+  rake TESTOPTS="--name $1"
+}
+function rake_test_with_file {
+  rake TEST=$1
+}
+
 alias rdbm='echo Migrating db and prepping test db; bundle exec rake db:migrate db:test:prepare'
 alias rdb='echo Dropping db, create, migrate, and test prepare, then seed;bundle exec rake db:drop db:create db:migrate db:test:prepare; bundle exec rake db:seed'
 alias rspec='bundle exec rspec'
 alias rake='bundle exec rake'
+alias rtn=rake_test_with_test_name
+alias rtf=rake_test_with_file
