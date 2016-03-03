2de5073c9a8a726f721d63dda354bc7a36670567
Greg Myers <neonmd@hotmail.co.uk>
2016-03-03 13:59:34 +0000

    Whoson, dnsflush(mac), raketestseed

--- rails.sh
+++ rails.sh
@@ -4,6 +4,9 @@ function rake_test_with_test_name {
 function rake_test_with_file {
   bundle exec rake TEST=$1
 }
+function rake_test_with_seed {
+  bundle exec rake TESTOPTS="--seed $1"
+}
 
 alias rdbm='echo Migrating db and prepping test db; bundle exec rake db:migrate db:test:prepare'
 alias rdb='echo Dropping db, create, migrate, and test prepare, then seed;bundle exec rake db:drop db:create db:migrate db:test:prepare; bundle exec rake db:seed'
@@ -11,3 +14,4 @@ alias rspec='bundle exec rspec'
 alias rake='bundle exec rake'
 alias rtn=rake_test_with_test_name
 alias rtf=rake_test_with_file
+alias rts=rake_test_with_seed
--- shell.sh
+++ shell.sh
@@ -1,9 +1,16 @@
+function whoson {
+  if ! lsof -i:${1}
+  then
+    echo "Nothing running on :${1}"
+  fi
+}
+
 alias ll='ls -al'
 alias l='ls -al'
-alias fndr='open /System/Library/CoreServices/Finder.app "$PWD"'
 alias reload='source ~/.bash_profile'
 alias sub='subl'
 alias serve='ruby -run -e httpd . -p 9090'
+alias dnsflush='sudo killall -HUP mDNSResponder'
 
 PS1='\[\033[0;33m\]$(ruby -v | cut -d " " -f 2)\[\033[0m\]:\W\[\033[0m\]$ '
 CLICOLOR=1; export CLICOLOR
